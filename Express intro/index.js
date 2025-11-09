ab38ba1599f6beae59f780a728ef4686620b225c
Paritosh <paritoshsharma2020@gmail.com>
2025-11-09 19:39:46 +0530

    Intro to express

---
+++ Express intro/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+  res.send("Hi from Server");
+});
+app.get("/about", (req, res) => {
+  res.send(`Hi ${req.query.name}`);
+});
+app.listen(port, () => console.log(`Server is listening! on ${port}`));
